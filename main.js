fcbd69d6bf33647885240a9779410697332ca30f
manjurulshawon <shawon3154@.com>
2023-08-10 23:40:17 +0600

    frist commit

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
  "use strict";
 
 // ***************************Spread Operator***************
-let poorcountry =['Bangladesh','Srilanka'];
-let rachCountry =[...poorcountry,'America','Canada','Italy'];
-console.log(rachCountry);
+// let poorCountry =['Bangladesh','Srilanka'];
+// let rachCountry =[...poorcountry,'America','Canada','Italy'];
+// console.log(rachCountry);
+
+//  without spread Operator - using Push()
+let poorCountry=['Bangladesh','Srilanka','Pakishthan'];
+let rachCountry=['America','Canada','Italy'];
+// rachCountry.push(poorCountry);
+
+// *********Rest Parameter***************
+
+function Calculation(a,b,...numbers){
+let sum = 0;
+for(let i of numbers){
+
+    sum=sum+i;
+}
+console.log(sum);
+}
+//  Calculation(2,4,6,5);
+
+// ****************  Dynamic Function
+
+let name=function(namevalue){
+  return namevalue;
+}
+console.log(name("Manjurul Islam Shawon"));
+
