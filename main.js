6fb8b21e3f70ac82f8792991dd864fe6ba6cb05e
manjurulshawon <shawon3154@.com>
2023-08-10 20:42:46 +0600

    frist commit

---
+++ main.js
@@ -0,0 +1,6 @@
+ "use strict";
+
+// ***************************Spread Operator***************
+let poorcountry =['Bangladesh','Srilanka'];
+let rachCountry =[...poorcountry,'America','Canada','Italy'];
+console.log(rachCountry);
